1952290d67925bd7fd63edccaa3f7cefbef33ee8
Sckaletz <allan@leeloo.dk>
2024-08-08 11:35:12 +0200

    Update

--- VisualAPI/obj/Debug/net8.0/VisualAPI.AssemblyInfo.cs
+++ VisualAPI/obj/Debug/net8.0/VisualAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisualAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26eaab3876d1d95f969b8d5953972439705d9f08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8c015308a416128d2ce928517b1d78bfdd8ce3")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisualAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisualAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
